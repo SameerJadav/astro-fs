903edd4f8dcf7fc9aad386885a0ac3868a40b841
SameerJadav <sameerjadav010@gmail.com>
2024-01-15 10:08:20 +0530

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+import { grayDark } from "@radix-ui/colors";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          1: grayDark.gray1,
+          2: grayDark.gray2,
+          3: grayDark.gray3,
+          4: grayDark.gray4,
+          5: grayDark.gray5,
+          6: grayDark.gray6,
+          7: grayDark.gray7,
+          8: grayDark.gray8,
+          9: grayDark.gray9,
+          10: grayDark.gray10,
+          11: grayDark.gray11,
+          12: grayDark.gray12,
+        },
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
