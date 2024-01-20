dd80ad6c0cc661b9c339e261532d80a51b2d2532
SameerJadav <sameerjadav010@gmail.com>
2024-01-20 20:21:13 +0530

    chore: adding remaining files

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+const config = {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
+
+export default config;
