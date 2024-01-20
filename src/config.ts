f872a892b7891fb230e1481f86c1ea0b4afd0d3d
SameerJadav <sameerjadav010@gmail.com>
2024-01-20 16:55:43 +0530

    feat: add genre carousel

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import HipHop_Poster from "~/images/posters/hip-hop.webp";
+import HybridTrap_Poster from "~/images/posters/hybrid-trap.webp";
+import Indie_Poster from "~/images/posters/indie.webp";
+import Lofi_Poster from "~/images/posters/lofi.webp";
+import Pop_Poster from "~/images/posters/pop.webp";
+import UKDrill_Poster from "~/images/posters/uk-drill.webp";
+
+export const POSTERS = [
+  { src: HipHop_Poster, alt: "Hip-Hop Poster", href: "/hiphop" },
+  { src: HybridTrap_Poster, alt: "Hybrid Trap Poster", href: "/hybrid-trap" },
+  { src: UKDrill_Poster, alt: "UK Drill Poster", href: "/uk-drill" },
+  { src: Pop_Poster, alt: "Pop Poster", href: "/pop" },
+  { src: Lofi_Poster, alt: "Lofi Poster", href: "/lofi" },
+  { src: Indie_Poster, alt: "Indie Poster", href: "/indie" },
+];
